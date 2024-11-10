0f21c45e36b4521f99b301825ec0be8563da1638
Mohammad Milhem <mohamadmilhem100@gmail.com>
2024-11-10 15:22:40 +0200

    Scanning: Parentheses.

--- app/main.js
+++ app/main.js
@@ -19,7 +19,19 @@ const filename = args[1];
 const fileContent = fs.readFileSync(filename, "utf8");
 
 if (fileContent.length !== 0) {
-  throw new Error("Scanner not implemented");
+  let lines = fileContent.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i].trim();
+    for (let j = 0; j < line.length; j++) {
+      if (line[j] === ')'){
+        console.log("RIGHT_PAREN ) null");
+      }
+      else if (line[j] === '('){
+        console.log("LEFT_PAREN ( null");
+      }
+    }
+  }
+  console.log("EOF  null");
 } else {
   console.log("EOF  null");
 }
